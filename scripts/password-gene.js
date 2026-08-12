bcbbc0125bd308b0458d5554be31e902bab4a672
Anton Strolz <tonystrolz@gmail.com>
2026-08-11 20:02:57 -0500

    chore: clean up FCC requirement comments

--- scripts/password-gene.js
+++ scripts/password-gene.js
@@ -1,18 +1,3 @@
-/*
-
-1. You should have a generatePassword function with a parameter. You can name the parameter whatever you like.
-2. Your generatePassword function should return a string.
-3. Your generatePassword function should return a new string that is the correct length.
-4. Your function should return a randomly generated password with valid characters.
-5. Your function should return a randomly generated password which contains more than one unique character.
-6. Your function should return a new random string each time it is called.
-7. You should have a password variable.
-8. Your password variable should be a string.
-9. You should call the generatePassword function with a numeric argument and store the returned password in the password variable.
-10. You should log a single string combining Generated password: and the password separated by a single space using + or a template literal.
-
-*/
-
 function generatePassword(num) {
     
     /* This creates the pool of selctable characters */
